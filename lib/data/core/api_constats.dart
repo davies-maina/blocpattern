6ff212b2c18c763836bff33d61862855fa0ab336
Davies Maina <daviesmaina@outlook.com>
2021-08-06 09:34:45 +0300

    bloc file structure

---
+++ lib/data/core/api_constats.dart
@@ -0,0 +1,5 @@
+class ApiConstants {
+  static const String BASE_URL = 'https://api.themoviedb.org/3/';
+  static const String API_KEY = 'a4770fc263f26cc67e2c5d90c32815cf';
+  static const String BASE_URL_IMAGE = 'https://image.tmdb.org/t/p/w500';
+}
